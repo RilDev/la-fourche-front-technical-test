bf62f7f33c2a8b2d6179b68cfc8889f1e47a4a63
RilDev <c.public@tutanota.com>
2022-05-31 20:08:59 +0200

    setup structure home page

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    domains: ["img.bbystatic.com"],
+  },
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
+      use: ["@svgr/webpack"],
+    });
 
-    return config
+    return config;
   },
-}
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,48 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch } from 'react-instantsearch-dom';
+import Head from "next/head";
+import Image from "next/image";
+import algoliasearch from "algoliasearch/lite";
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+} from "react-instantsearch-dom";
+import Logo from "public/logo.svg";
+import AddCartIcon from "public/add-cart-icon.svg";
+import RemoveCartIcon from "public/remove-cart-icon.svg";
+
+function Hit({ hit }) {
+  return (
+    <div>
+      <Image src={hit.image} width={50} height={50} />
+      <div>{hit.name}</div>
+      <div>{hit.salePrice}</div>
+      <AddCartIcon />
+      <RemoveCartIcon />
+    </div>
+  );
+}
 
 const searchClient = algoliasearch(
-  'latency',
-  '6be0576ff61c053d5f9a3225e2a90f76'
+  "latency",
+  "6be0576ff61c053d5f9a3225e2a90f76"
 );
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>La Fourche - Product Finder</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to La Fourche Frontend Technical Test v2 !
-        </h1>
-        <InstantSearch
-          indexName="bestbuy"
-          searchClient={searchClient}
-        >
-          {/* ##################################
-              Your instantsearch code goes here.
-              ################################## */}
-        </InstantSearch>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      <InstantSearch indexName="bestbuy" searchClient={searchClient}>
+        <nav className="flex px-2 py-4 shadow-md w-full">
+          <Logo />
+          <SearchBox />
+        </nav>
+        <Hits hitComponent={Hit} className="bg-red-500 grid columns-2" />
+        <Pagination className="flex" />
+      </InstantSearch>
+    </>
+  );
 }
